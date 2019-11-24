f5a64a218876847f8528fcbb9fee72acc5f955f7
soundarig <gnanasoundari.soundarajan@emc.com>
2019-11-24 14:58:03 +0530

    Adding gender, viewsum,users as optional parameter

--- src/main/scala/model/JsonModels.scala
+++ src/main/scala/model/JsonModels.scala
@@ -2,6 +2,6 @@ package model
 
 case class User(registertime: Long, userid: String, regionid: String, gender: String)
 
-case class PageViews(viewtime: Long, userid: String, pageid: String)
+case class PageViews(viewtime: Long, userid: String, pageid: String, sum: Option[Long], users: Option[String], gender: Option[String])
 
 case class TopViews(pageid: String, gender: String, viewtime: Long, distinctUserCount: Long)
